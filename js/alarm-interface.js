b67248aa1580ea2142bca5110fef0d4968041cdc
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-09-07 12:30:16 +0300

    Display current day, date, month, year, hour, minute and seconds

--- js/alarm-interface.js
+++ js/alarm-interface.js
@@ -1,9 +1,13 @@
 var displayTime = function() {
-    var time = moment().format('HH:mm:ss');
+    var time = moment().format('dddd , DD-MM-YYYY , HH:mm:ss');
+    $('#currentTime').text(time);
+    setInterval(displayTime, 1000);
 };
 
 $(document).ready(function () {
-    console.log('Okay, locked and loaded');
+    // Display current time
+    displayTime();
+
     $('#setAlarmForm').click(function (event) {
         event.preventDefault();
         console.log("I am grroot");
