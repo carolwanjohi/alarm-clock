8f8b0b0b453ac10314f0ae9ff19905d18f363005
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-09-07 11:33:25 +0300

    Create js files and updat index

---
+++ js/alarm-interface.js
@@ -0,0 +1,11 @@
+var displayTime = function() {
+    var time = moment().format('HH:mm:ss');
+}
+
+$(document).ready(function () {
+    console.log('Okay, locked and loaded');
+    $('#setAlarmForm').click(function (event) {
+        event.preventDefault();
+        console.log("I am grroot");
+    });
+});
